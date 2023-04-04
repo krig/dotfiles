d4a55f1e5166425dd2000f9457471d36612e670e
Kristoffer Grönlund <kristoffer.gronlund@netinsight.net>
2023-04-04 10:02:08 +0200

    bigger font in kitty and more visual cues for leap

--- nvim/init.lua
+++ nvim/init.lua
@@ -298,6 +298,11 @@ require('flit').setup {
   opts = {}
 }
 
+-- Greying out the search area
+-- Or just set to grey directly, e.g. { fg = '#777777' },
+-- if Comment is saturated.
+vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+
 local orgmode = require 'orgmode'
 orgmode.setup_ts_grammar()
 orgmode.setup()
