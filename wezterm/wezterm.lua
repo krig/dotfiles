744aa10d68bd34031e34fb636b0e4c2b48127f75
Kristoffer Gronlund <kristoffer.gronlund@netinsight.se>
2023-05-31 12:51:04 +0200

    update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,26 @@
-local wezterm = require('wezterm')
-return {
-	font = wezterm.font 'Cascadia Mono',
-	font_size = 16,
-	color_scheme = 'Rosé Pine (base16)',
+local wezterm = require("wezterm")
+
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font("Cascadia Mono PL")
+config.font_size = 16
+config.color_scheme = "rose-pine-moon"
+
+config.keys = {
+	{
+		key = "|",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "@",
+		mods = "CMD",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 }
+
+return config
