7a48abfa07739e6979c97f61af25f943ee712433
Kristoffer Grönlund <kristoffer.gronlund@netinsight.net>
2023-10-16 15:25:29 +0200

    very sick

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -15,15 +15,21 @@ return {
         oldfiles = {
           theme = "ivy",
         },
+        live_grep = {
+          theme = "ivy",
+        },
+        grep_string = {
+          theme = "ivy",
+        },
       },
     },
     keys = {
-      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-      { "<leader><space>", "<cmd>Telescope files<cr>", desc = "Find Files" },
+      { "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+      { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+      { "<leader>ss", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+      { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
       { "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "Git Files" },
       { "<leader>sw", "<cmd>Telescope grep_string word_match=-w<cr>", desc = "Grep Word" },
-      { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
-      { "<leader>ss", "<leader>sg", desc = "Grep" },
     },
   },
 }
