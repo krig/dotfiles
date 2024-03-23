56c72612ec8a9f69874788220568b0f8a3fa1415
Kristoffer Grönlund <k@ziran.se>
2024-03-23 15:28:10 +0100

    switch back to iosevka

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,8 +6,8 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.font = wezterm.font("Operator-caska")
-config.font_size = 15
+config.font = wezterm.font("Iosevka Term SS07")
+config.font_size = 16
 -- config.color_scheme = "Catppuccin Mocha"
 config.color_scheme = "Catppuccin Frappé (Gogh)"
 
