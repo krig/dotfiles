2d4d938564bceb8feec476a7bd17a2966912ebd1
Kristoffer Gronlund <kristoffer.gronlund@netinsight.net>
2024-04-17 15:52:37 +0200

    Some new telescope bindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,9 +4,12 @@ return {
     keys = {
       { "<leader>t", "<cmd>Telescope oldfiles<cr>", desc = "Recent Files" },
       { "<leader>n", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+      { "<leader>\"", "<cmd>Telescope registers<cr>", desc = "Grep" },
       { "<leader>ss", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
       { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
       { "<leader>gf", "<cmd>Telescope git_files<cr>", desc = "Git Files" },
+      { "<leader>gb", "<cmd>Telescope git_bcommits<cr>", desc = "Git Buffer Commits" },
+      { "<leader>gt", "<cmd>Telescope git_branches<cr>", desc = "Git Branches" },
       { "<leader>sw", "<cmd>Telescope grep_string word_match=-w<cr>", desc = "Grep Word" },
     },
   },
