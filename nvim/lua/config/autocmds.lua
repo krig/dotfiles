c34c0f2b25cdd6d26bb2ba9d565c6b58fdb15505
Kristoffer Gronlund <kristoffer.gronlund@netinsight.net>
2024-05-27 14:48:39 +0200

    this seems to work to disable lsp highlighting

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -13,3 +13,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if (client ~= nil) then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
+  end,
+})
--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -110,6 +110,8 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+      document_highlight = { enabled = false },
+      codelens = { enabled = false },
     },
   },
 }
