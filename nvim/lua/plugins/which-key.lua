47175123c9487a2b7a2426241975eb4f942bdc77
Kristoffer Gronlund <kristoffer.gronlund@netinsight.net>
2024-08-12 09:54:10 +0200

    disable which-key for now

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,38 +1,29 @@
 return {
   {
     "folke/which-key.nvim",
+    enabled = false,
     event = "VeryLazy",
     opts_extend = { "spec" },
     opts = {
-      icons = { mappings = false },
       defaults = {},
+      icons = { mappings = false },
       spec = {
         {
           mode = { "n", "v" },
+          { "<leader>b", group = "buffer" },
           { "<leader>c", group = "code" },
           { "<leader>f", group = "file" },
           { "<leader>g", group = "git" },
           { "<leader>q", group = "quit" },
           { "<leader>s", group = "search" },
-          { "<leader>u", group = "ui", icon = { color = "cyan" } },
-          { "<leader>x", group = "diagnostics/quickfix", icon = { color = "green" } },
-          {
-            "<leader>b",
-            group = "buffer",
-            expand = function()
-              return require("which-key.extras").expand.buf()
-            end,
-          },
+          { "<leader>t", group = "terminal" },
+          { "<leader>u", group = "undo" },
+          { "<leader>x", group = "trouble" },
           {
             "<leader>w",
             group = "windows",
             proxy = "<c-w>",
-            expand = function()
-              return require("which-key.extras").expand.win()
-            end,
           },
-          -- better descriptions
-          { "gx", desc = "Open with system app" },
         },
       },
     },
