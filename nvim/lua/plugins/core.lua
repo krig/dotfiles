4a324217a698ee0071e3ba188491aff27d83664b
Kristoffer Gronlund <kristoffer.gronlund@netinsight.net>
2024-09-24 11:38:49 +0200

    move lualine to separate file

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,24 +1,6 @@
 -- Using this to add small plugins instead of
 -- creating a separate file for each one.
 
--- get project-relative path of buffer
-local function relpath()
-  local bufname = vim.api.nvim_buf_get_name(0)
-  local name = string.gsub(bufname, vim.loop.cwd(), '@')
-  if name:sub(1, #'@') == '@' then
-    return name
-  end
-  name = string.gsub(bufname, vim.env.HOME, '~')
-  if name ~= '' then
-    return name
-  end
-  name = vim.fn.expand('%')
-  if name ~= '' then
-    return name
-  end
-  return '[no name]'
-end
-
 return {
   {
     "tpope/vim-sleuth",
@@ -64,34 +46,6 @@ return {
     lazy = false,
     keys = {
       { "<leader>ut", vim.cmd.UndotreeToggle, desc = "Toggle Undotree" },
-    },
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = "auto",
-        globalstatus = true,
-        section_separators = '',
-        component_separators = '',
-      },
-      sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'searchcount', 'selectioncount'},
-        lualine_c = {relpath},
-        lualine_x = {},
-        lualine_y = {'diagnostics'},
-        lualine_z = {'branch'}
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-      },
     },
   },
   {
---
+++ nvim/lua/plugins/lualine.lua
@@ -0,0 +1,48 @@
+-- get project-relative path of buffer
+local function relpath()
+  local bufname = vim.api.nvim_buf_get_name(0)
+  local name = string.gsub(bufname, vim.loop.cwd(), '@')
+  if name:sub(1, #'@') == '@' then
+    return name
+  end
+  name = string.gsub(bufname, vim.env.HOME, '~')
+  if name ~= '' then
+    return name
+  end
+  name = vim.fn.expand('%')
+  if name ~= '' then
+    return name
+  end
+  return '[no name]'
+end
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = "auto",
+        globalstatus = true,
+        section_separators = '',
+        component_separators = '',
+      },
+      sections = {
+        lualine_a = { { 'mode', fmt = string.lower } },
+        lualine_b = {'searchcount', 'selectioncount'},
+        lualine_c = {relpath},
+        lualine_x = {},
+        lualine_y = {'diagnostics'},
+        lualine_z = { { 'branch', color = { fg = '#949cbb', bg = '#292c3c' } } }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {'filename'},
+        lualine_x = {'location'},
+        lualine_y = {},
+        lualine_z = {}
+      },
+    },
+  },
+}
