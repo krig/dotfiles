6365a5d110b5ed6d5ac1707957c57b50d6e6bed5
Kristoffer Gronlund <kristoffer.gronlund@netinsight.net>
2024-10-09 13:31:40 +0200

    better link implementation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -59,4 +59,22 @@ config.keys = {
 	},
 }
 
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- match work bug numbers
+table.insert(config.hyperlink_rules, {
+  regex = [[\bGAS-(\d+)\b]],
+  format = 'https://netinsight.atlassian.net/browse/GAS-$1',
+})
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+  format = 'https://www.github.com/$1/$3',
+})
+
 return config
