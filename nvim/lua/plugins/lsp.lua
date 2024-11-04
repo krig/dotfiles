92803275ff8eb16fbe17d557d07583c3eaf576eb
Kristoffer Grönlund <k@ziran.se>
2024-11-04 09:35:21 +0100

    linting

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,15 +4,107 @@ return {
     event = { "BufReadPost", "BufWritePost", "BufNewFile" },
     dependencies = {
       {
-          "SmiteshP/nvim-navbuddy",
-          dependencies = {
-              "SmiteshP/nvim-navic",
-              "MunifTanjim/nui.nvim"
-          },
-          opts = { lsp = { auto_attach = true } },
+        "SmiteshP/nvim-navbuddy",
+        dependencies = {
+          "SmiteshP/nvim-navic",
+          "MunifTanjim/nui.nvim",
+        },
+        opts = { lsp = { auto_attach = true } },
       },
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
+      {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+        opts = {
+          events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+          linters_by_ft = {
+            python = { "ruff" },
+            sql = { "sqlfluff" },
+          },
+          linters = {
+            sqlfluff = {
+              args = {
+                'lint', '--format=json', '--dialect=sqlite',
+              },
+            },
+          },
+        },
+        config = function(_, opts)
+          local M = {}
+
+          local lint = require "lint"
+          for name, linter in pairs(opts.linters) do
+            if type(linter) == "table" and type(lint.linters[name]) == "table" then
+              lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
+              if type(linter.prepend_args) == "table" then
+                lint.linters[name].args = lint.linters[name].args or {}
+                vim.list_extend(lint.linters[name].args, linter.prepend_args)
+              end
+            else
+              lint.linters[name] = linter
+            end
+          end
+          lint.linters_by_ft = opts.linters_by_ft
+
+          function M.debounce(ms, fn)
+            local timer = vim.uv.new_timer()
+            return function(...)
+              local argv = { ... }
+              timer:start(ms, 0, function()
+                timer:stop()
+                vim.schedule_wrap(fn)(unpack(argv))
+              end)
+            end
+          end
+
+          function M.lint()
+            -- Use nvim-lint's logic first:
+            -- * checks if linters exist for the full filetype first
+            -- * otherwise will split filetype by "." and add all those linters
+            -- * this differs from conform.nvim which only uses the first filetype that has a formatter
+            local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+
+            -- Create a copy of the names table to avoid modifying the original.
+            names = vim.list_extend({}, names)
+
+            -- Add fallback linters.
+            if #names == 0 then
+              vim.list_extend(names, lint.linters_by_ft["_"] or {})
+            end
+
+            -- Add global linters.
+            vim.list_extend(names, lint.linters_by_ft["*"] or {})
+
+            -- Filter out linters that don't exist or don't match the condition.
+            local ctx = { filename = vim.api.nvim_buf_get_name(0) }
+            ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
+            names = vim.tbl_filter(function(name)
+              local linter = lint.linters[name]
+              if not linter then
+                vim.print("Linter not found: " .. name)
+              end
+              return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
+            end, names)
+
+            -- Run linters.
+            if #names > 0 then
+              lint.try_lint(names)
+            end
+          end
+
+          vim.api.nvim_create_autocmd(opts.events, {
+            group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
+            callback = M.debounce(100, M.lint),
+          })
+        end,
+      },
+      {
+        "rshkarin/mason-nvim-lint",
+        opts = {
+          ensure_installed = { "sqlfluff", "bacon", "ruff" },
+        },
+      },
     },
     keys = {
       { "<leader>n", "<cmd>Navbuddy<cr>", desc = "Navbuddy" },
@@ -69,14 +161,13 @@ return {
             settings = {
               Lua = {
                 diagnostics = {
-                  globals = { 'vim' },
+                  globals = { "vim" },
                 },
               },
             },
           },
         },
-        setup = {
-        },
+        setup = {},
       }
     end,
     config = function(_, opts)
@@ -134,14 +225,10 @@ return {
       end
 
       if have_mason then
-        mlsp.setup({
-          ensure_installed = vim.tbl_deep_extend(
-            "force",
-            ensure_installed,
-            {}
-          ),
+        mlsp.setup {
+          ensure_installed = vim.tbl_deep_extend("force", ensure_installed, {}),
           handlers = { setup },
-        })
+        }
       end
     end,
   },
@@ -161,14 +248,14 @@ return {
     },
     config = function(_, opts)
       require("mason").setup(opts)
-      local mr = require("mason-registry")
+      local mr = require "mason-registry"
       mr:on("package:install:success", function()
         vim.defer_fn(function()
           -- trigger FileType event to possibly load this newly installed LSP server
-          require("lazy.core.handler.event").trigger({
+          require("lazy.core.handler.event").trigger {
             event = "FileType",
             buf = vim.api.nvim_get_current_buf(),
-          })
+          }
         end, 100)
       end)
 
