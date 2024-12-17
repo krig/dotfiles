3a2b16af6294030f99f8187ffb3d260ad22937cd
Kristoffer Grönlund <k@ziran.se>
2024-12-17 01:54:59 +0100

    JetBrains Mono without ligatures

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,9 +6,24 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
---config.font = wezterm.font("Iosevka Term SS07")
+config.font = wezterm.font_with_fallback {
+  {
+    family = 'JetBrains Mono',
+    harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+  },
+  {
+    family = 'Fira Code',
+    harfbuzz_features = { 'clig=0', 'liga=0' },
+  },
+  {
+    family = 'Operator-caska',
+  },
+  'Iosevka Term SS07',
+  'JetBrains Mono',
+}
+-- config.font = wezterm.font("Iosevka Term SS07")
 -- config.font = wezterm.font("Operator-caska")
-config.font = wezterm.font("Fira Code")
+-- config.font = wezterm.font("Fira Code")
 config.font_size = 16
 config.color_scheme = "carbonfox"
 -- config.color_scheme = "Catppuccin Mocha"
