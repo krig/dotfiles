b0b32ec8085889fbbb1cc35827b50f758e1ece44
Kristoffer Grönlund <k@ziran.se>
2024-12-29 15:59:07 +0100

    basic path

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+eval (/opt/homebrew/bin/brew shellenv)
+
+fish_add_path -p $HOME/.cargo/bin
+fish_add_path -p $HOME/bin
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
