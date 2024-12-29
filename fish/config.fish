2e2bba107e8e8f9e26ad1c2657553e51e28767d2
Kristoffer Grönlund <k@ziran.se>
2024-12-29 23:32:58 +0100

    I think this pretty much works

--- fish/config.fish
+++ fish/config.fish
@@ -1,48 +1,67 @@
+set -g fish_greeting "... ><^,⋗"
+
 eval (/opt/homebrew/bin/brew shellenv)
 
-fish_add_path -p /opt/homebrew/bin
-fish_add_path -p $HOME/.cargo/bin
-fish_add_path -p $HOME/bin
+fish_add_path /opt/homebrew/bin
+
+if test -d /opt/homebrew/opt/ruby/bin
+  fish_add_path /opt/homebrew/opt/ruby/bin
+end
+
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/bin
 
 set -gx LANG en_US.UTF-8
 set -gx QMK_HOME $HOME/src/qmk_firmware
 set -gx EDITOR nvim
 set -gx BAT_THEME base16
 
+set -gx NAME "Kristoffer Grönlund"
+set -gx EMAIL "k@ziran.se"
+
+set -gx AWS_PROFILE mfa
+
+if test -f $HOME/.env
+  . (sed 's/^/export /' $HOME/.env | psub)
+end
+
+if test -f $HOME/.cargo/env.fish
+  source $HOME/.cargo/env.fish
+end
+
 if status is-interactive
-    function k --wraps=kubectl --description 'alias k kubectl'
-      kubectl $argv
-    end
+  abbr --add k kubectl
 
-    function shu --wraps=ssh --description 'ssh unknown'
-      ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $argv
-    end
+  abbr --add t tig
+  abbr --add g git
+  abbr --add gp git pull
+  abbr --add gfa git fetch --all
+  abbr --add gco git commit
+  abbr --add gpf git push --force-with-lease
+  abbr --add gau git autosquash
+  abbr --add gab git absorb
+  abbr --add gch git checkout
+  abbr --add ga git add
 
-    function t --wraps=tig --description 'alias t tig'
-      tig $argv
-    end
+  abbr --add vim nvim
+  abbr --add vi nvim
+  abbr --add e nvim
 
-    function g --wraps=git --description 'alias g gig'
-      git $argv
-    end
 
-    function vim --wraps=nvim --description 'alias vim nvim'
-      nvim $argv
-    end
+  function shu --wraps=ssh --description 'ssh unknown'
+    ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $argv
+  end
 
-    function e --wraps=nvim --description 'alias e nvim'
-      nvim $argv
-    end
-
+  if command -q lsd
     function ls --wraps=lsd --description 'alias ls lsd'
       lsd $argv
     end
-
-    function ll --wraps=lsd --description 'alias ll lsd -l'
-      lsd -l $argv
+  else
+    function ls
+      command ls --color=auto -p $argv
     end
+  end
 
-    function la --wraps=lsd --description 'alias la lsd -l'
-      lsd -a $argv
-    end
+  abbr --add ll ls -l
+  abbr --add la ls -a
 end
