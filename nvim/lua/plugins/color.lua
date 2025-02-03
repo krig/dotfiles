19454950221f111c0571024b68b73d4fd8dadd05
Kristoffer Gronlund <kristoffer.gronlund@netinsight.net>
2025-02-03 11:29:16 +0100

    Try melange

---
+++ nvim/lua/plugins/color.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "savq/melange-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.opt.termguicolors = true
+      vim.cmd.colorscheme 'melange'
+    end,
+  },
+}
