c12ec21330740428e6b4f9191b01cd12b0e316b8
Kristoffer Gronlund <kristoffer.gronlund@netinsight.net>
2025-05-26 11:04:26 +0200

    Revert "Switch to main branch for nvim-treesitter"
    This reverts commit fb3eab9a50471801b4ed98bc753bc1c37f8a78c7.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,44 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    lazy = 'false',
     build = ":TSUpdate",
     config = function()
-      require'nvim-treesitter'.install {
-        "bash",
-        "c",
-        "css",
-        "dockerfile",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "odin",
-        "python",
-        "query",
-        "roc",
-        "sql",
-        "superhtml",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "zig",
+      require 'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+          "bash",
+          "c",
+          "css",
+          "dockerfile",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "odin",
+          "python",
+          "query",
+          "roc",
+          "sql",
+          "superhtml",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "zig",
+        },
+        modules = {},
+        sync_install = false,
+        ignore_install = {},
+        auto_install = false,
+
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
       }
     end,
   },
